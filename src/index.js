edfc452a0f4072115c82d43417e8a1b0683f4239
darcio-codes279 <darciomassala@gmail.com>
2023-03-06 19:28:44 +0000

    1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import Layout from './components/Layout/Layout';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Layout>
     <App />
+    </Layout>
+    
   </React.StrictMode>
 );
 
